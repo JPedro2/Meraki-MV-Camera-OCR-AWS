cfe7c2528a9e57cdfa68780e4a1e9f0fcb2f8343
JPedro2 <peolivei@cisco.com>
2020-02-29 22:33:44 +0000

    Adding Coffee Demo capabilities with IFTTT and Smarter

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 from flask import request, Flask
 import Meraki_AWS_Script
 import credentials
+import coffee_demo
 
 flask_app = Flask(__name__)
 
@@ -10,6 +11,13 @@ def detect_text():
     Meraki_AWS_Script.snapshot_meraki_camera()
     detected_text = Meraki_AWS_Script.detect_text()
     return detected_text
+
+@flask_app.route("/brew-coffee", methods=["GET"])
+def brew_coffee_demo():
+    fatigue = coffee_demo.get_fatigue_result()
+    coffee_strength = coffee_demo.strength_calc(fatigue)
+    make_coffee = coffee_demo.IFTTT_make_coffee(coffee_strength)
+    return make_coffee
 
 
 if __name__ == "__main__":
